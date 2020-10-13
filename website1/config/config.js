4651654242476934f0a490316def823e94fafc2f
sumeetrk <47571705+sumeetrk@users.noreply.github.com>
2020-10-13 17:46:31 +0530

    Update config.js

--- website1/config/config.js
+++ website1/config/config.js
@@ -4,6 +4,6 @@ const config = {
     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
     mongoUri: process.env.MONGODB_URI ||
     process.env.MONGO_HOST ||
-    "mongodb+srv://sumeet:CDANRXUlU9usXbKF@cluster0.kkdsq.gcp.mongodb.net/Social?retryWrites=true&w=majority"
+    "<mongodburi"
    }
    export default config
